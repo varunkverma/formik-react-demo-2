4082e051ae49748d1f00580ed4915f6349c509fe
varunkverma <varunkverma1996@gmail.com>
2020-05-03 19:33:58 +0530

    basic setup

---
+++ src/components/youtube-form.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const YouTubeForm = () => {
+  return (
+    <div className="">
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+
+        <label htmlFor="channel">Channel</label>
+        <input type="text" name="channel" id="channel" />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default YouTubeForm;
