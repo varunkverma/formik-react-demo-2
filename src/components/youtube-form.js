64c02aa5cf43e45542bfb96dbd8552a1268be1c0
varunkverma <varunkverma1996@gmail.com>
2020-05-03 20:28:15 +0530

    formstate and form submission

--- src/components/youtube-form.js
+++ src/components/youtube-form.js
@@ -1,19 +1,48 @@
 import React from "react";
+import { useFormik } from "formik";
 
 const YouTubeForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div className="">
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" name="channel" id="channel" />
+        <input
+          type="text"
+          name="channel"
+          id="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
 
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
